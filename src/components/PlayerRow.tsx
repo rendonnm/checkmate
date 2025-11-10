7d0e7601417ff0f72fda3051b4ca237dd3bc18f1
RendonnM <srendonm@correo.iue.edu.co>
2025-11-09 21:15:15 -0500

    feat(PlayerRow)✨: add className prop to PlayerRow for customizable styling

--- src/components/PlayerRow.tsx
+++ src/components/PlayerRow.tsx
@@ -1,10 +1,11 @@
 interface PlayerRowProps {
 	name: string;
+	className: string;
 }
 
-export const PlayerRow = ({ name }: PlayerRowProps) => {
+export const PlayerRow = ({ name, className }: PlayerRowProps) => {
 	return (
-		<article className="ml-8 text-white h-10">
+		<article className={`ml-8 text-white ${className} h-10`}>
 			<p className="font-semibold">{name}</p>
 		</article>
 	);
