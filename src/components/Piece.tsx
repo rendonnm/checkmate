410b3d27c10efabf0cb067d7d17c5d2bf7390156
RendonnM <srendonm@correo.iue.edu.co>
2025-11-09 21:15:24 -0500

    style(Piece)💄: enhance Piece component with cursor and background styles for better user interaction

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -4,8 +4,8 @@ import { PIECE_SVG } from "@/constants/pieces";
 export const Piece = ({ piece, color }: PieceDataProps) => {
 	const Svg = PIECE_SVG[color][piece];
 	return (
-		<div className="size-full">
-			<Svg />
+		<div className="size-full cursor-grab active:bg-blue-400 dragging:opacity-50">
+			<Svg className="shadow" />
 		</div>
 	);
 };
